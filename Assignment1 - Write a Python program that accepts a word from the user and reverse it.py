5b139afc7109689882b06518d45ccdc75a8ed8f4
rupendrag <123197498+rupendrag@users.noreply.github.com>
2023-09-28 03:43:33 +0530

    Update Assignment1 - Write a Python program that accepts a word from the user and reverse it.py

--- Assignment1 - Write a Python program that accepts a word from the user and reverse it.py
+++ Assignment1 - Write a Python program that accepts a word from the user and reverse it.py
@@ -1,5 +1,5 @@
 #Write a Python program that accepts a word from the user and reverse it.
 
 word = input("Enter Word")
-reversse_word = word[::-1]
-print(reversse_word)
+reverse_word = word[::-1]
+print(reverse_word)
