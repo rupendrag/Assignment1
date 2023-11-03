900a4ebf3cb917fdb7abd9d6f97a61c3fb096e4f
rupendrag <123197498+rupendrag@users.noreply.github.com>
2023-11-03 23:18:48 +0530

    Update Assignment 5 - Challenge3.py

--- Assignment 5 - Challenge3.py
+++ Assignment 5 - Challenge3.py
@@ -1,14 +1,32 @@
-class Account:
-    def __init__(self, title, balance):
-        self.title = title
-        self.balance = balance
+class Student:
+    def _init_(self):
+        self.__name = None
+        self.__roll_number = None
+    def getName(self):
+        return self.__name
+    def getRollNumber(self):
+        return self.__roll_number
+    def setName(self, name):
+        if isinstance(name, str):
+            self.__name = name
+        else:
+            raise ValueError("Invalid input for name. Name must be a string.")
+        
+    def setRollNumber(self, roll_number):
+        if isinstance(roll_number, str):
+            self.__roll_number = roll_number
+        else:
+            raise ValueError("Invalid input for roll number. Roll number must be a string.")
 
-class SavingsAccount(Account):
-    def __init__(self, title, balance, interest_rate):
-        super().__init__(title, balance)
-        self.interest_rate = interest_rate
+student = Student()
 
-savings_account = SavingsAccount("Aashish", 5000, 5)
-print(f"Account Title: {savings_account.title}")
-print(f"Account Balance: ${savings_account.balance}")
-print(f"Interest Rate: {savings_account.interest_rate}%")
+try:
+    name = input("Enter Students name ")
+    student.setName("Rupendra")
+    roll_number=input("Enter Roll number")
+    student.setRollNumber("123456")
+
+    print(f"Name: {student.getName()}")
+    print(f"Roll Number: {student.getRollNumber()}")
+except ValueError as e:
+    print(e)
